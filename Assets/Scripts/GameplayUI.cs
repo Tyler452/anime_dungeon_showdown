30c24f394ea0a6adaf96b94c75f8691f661556a2
sbagos27 <sbagos@csumb.edu>
2026-05-03 19:22:51 -0700

    commit base ability UI

---
+++ Assets/Scripts/GameplayUI.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+// this will be the class that handles the UI for Abilities
+public class GameplayUI : MonoBehaviour {
+    public GameObject Ability1;
+    public GameObject Ability2;
+    
+    public GameObject Ability3;
+    public GameObject Ability4;
+    
+    void Start()
+    {
+        
+    }
+
+    void ability1Used() {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
